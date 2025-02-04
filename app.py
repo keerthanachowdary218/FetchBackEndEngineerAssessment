b563d16b6bd6947beeb6d38565941d24b5590d28
KeerthanaGolla58 <60168668+keerthanachowdary218@users.noreply.github.com>
2025-02-04 13:53:19 -0500

    Removing my personal documentation

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def get_points(receipt_id):
     return jsonify({'points': points}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
